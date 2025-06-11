5819114d38e52e2ba3c91e78ebdd8737c19a1309
fVaqueroG <33148541+fVaqueroG@users.noreply.github.com>
2025-06-11 05:01:40 -0600

    Update mirage.h

--- components/mirage/mirage.h
+++ components/mirage/mirage.h
@@ -1,36 +1,37 @@
 #pragma once
 
 #include "esphome/components/climate_ir/climate_ir.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/remote_transmitter/remote_transmitter.h"
 
 namespace esphome {
 namespace mirage {
 
-/// Simple enum to represent models.
-
-// Temperature
-const float MIRAGE_TEMP_MAX = 32.0;
-const float MIRAGE_TEMP_MIN = 16.0;
-
 class MirageClimate : public climate_ir::ClimateIR {
  public:
-  uint8_t swing_position = 0;
-  MirageClimate()
-      : climate_ir::ClimateIR(MIRAGE_TEMP_MIN, MIRAGE_TEMP_MAX, 1.0f, true, true,
-                              {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM,
-                               climate::CLIMATE_FAN_HIGH},
-                              {climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_VERTICAL, climate::CLIMATE_SWING_HORIZONTAL, climate::CLIMATE_SWING_BOTH}) {}
+  explicit MirageClimate() : climate_ir::ClimateIR(16.0f, 32.0f, 1.0f) {}
 
-  void setup() override {
-    climate_ir::ClimateIR::setup();
+  void set_transmitter(remote_transmitter::RemoteTransmitterComponent *transmitter) {
+    transmitter_ = transmitter;
   }
 
+  void set_sensor(sensor::Sensor *sensor) {
+    sensor_ = sensor;
+  }
+
+  void setup() override;
+
+  climate::ClimateTraits traits() override;
+
+  void control(const climate::ClimateCall &call) override;
+
  protected:
-  /// Transmit via IR the state of this climate controller.
   void transmit_state() override;
-  /// Handle received IR Buffer
-  bool on_receive(remote_base::RemoteReceiveData data) override;
-  /// Set the time of the last transmission.
-  int32_t last_transmit_time_{};
+  bool on_receive(remote_base::RemoteReceiveData data);
+
+  remote_transmitter::RemoteTransmitterComponent *transmitter_{nullptr};
+  sensor::Sensor *sensor_{nullptr};
+  uint32_t last_transmit_time_{0};
 };
 
 }  // namespace mirage
